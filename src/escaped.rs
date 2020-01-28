b740d72bcef2920a77a89a3625dca5c68eee70f5
Kaleb Elwert <belak@coded.io>
2020-01-28 00:53:49 -0800

    Add comment to escaped.rs

--- src/escaped.rs
+++ src/escaped.rs
@@ -1,3 +1,12 @@
+// This file implements helpers for tag escaping and unescaping. Each function
+// lists all the possible escape characters, even if the fallback would be
+// valid.
+//
+// See https://ircv3.net/specs/extensions/message-tags.html for more
+// information.
+
+// Takes a single character and returns a str representing what to replace it
+// with or None if it can be used verbatim.
 #[inline(always)]
 pub(crate) fn escape_char(c: char) -> Option<&'static str> {
     match c {
@@ -10,6 +19,8 @@ pub(crate) fn escape_char(c: char) -> Option<&'static str> {
     }
 }
 
+// Takes the char after an escape character and returns what it should be
+// decoded as.
 #[inline(always)]
 pub(crate) fn unescape_char(c: char) -> char {
     match c {
